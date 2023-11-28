b5b64fe6fb4e3cb99ecfc624877ac316cad0850d
Bruhnodev17 <manoelbrunop17@gmail.com>
2023-11-28 00:08:41 -0300

    Manutenção ná lógica da aplicação

--- script.js
+++ script.js
@@ -1,50 +1,70 @@
-const buttonConvert = document.querySelector(".convert-button")
-const currencySelect = document.querySelector(".currency-select")
-
+const buttonConvert = document.querySelector(".convert-button");
+const currencySelect = document.querySelector(".currency-select");
+let dolarToday, euroToday, poundToday; 
 
 async function convertValues() {
-    const inputCurrencyValue = document.querySelector(".input-currency").value
-    const currencyValueToConvert = document.querySelector('.currency-value-to-convert') 
-    const currencyValueConverted = document.querySelector('.currency-value') 
+  const inputCurrencyValue = document.querySelector(".input-currency").value;
+  const currencyValueToConvert = document.querySelector(
+    ".currency-value-to-convert"
+  );
+  const currencyValueConverted = document.querySelector(".currency-value");
 
-    const data = await fetch(" https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL").then(response => response.json())
-    console.log(data)
-    
-    if (currencySelect.value == "dolar") { 
-        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
-            style: "currency",
-            currency: "USD"
-        }).format(inputCurrencyValue / dolarToday)
-    }
+  const data = await fetch(
+    "https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,GBP-BRL,BTC-BRL"
+  ).then((response) => response.json());
+  console.log(data);
 
-    if (currencySelect.value == "euro") { 
-        currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
-            style: 'currency',
-            currency: "EUR"
-        }).format(inputCurrencyValue / euroToday)
-    }
+  dolarToday = data.USDBRL.ask;
+  euroToday = data.EURBRL.ask;
+  poundToday = data.GBPBRL.ask;
 
-    currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
-        style: "currency",
-        currency: "BRL"
-    }).format(inputCurrencyValue)
+  if (currencySelect.value === "dolar") {
+    currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(inputCurrencyValue / dolarToday);
+  }
 
+  if (currencySelect.value === "euro") {
+    currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+      style: "currency",
+      currency: "EUR",
+    }).format(inputCurrencyValue / euroToday);
+  }
+
+  if (currencySelect.value === "libra") {
+    currencyValueConverted.innerHTML = new Intl.NumberFormat("en-GB", {
+      style: "currency",
+      currency: "GBP",
+    }).format(inputCurrencyValue / poundToday);
+  }
+
+  currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  }).format(inputCurrencyValue);
 }
 
 function changeCurrency() {
-    const currencyName = document.getElementById('currency-name')
-    const currencyImage = document.querySelector('.currency-image')
+  const currencyName = document.getElementById("currency-name");
+  const currencyImage = document.querySelector(".currency-image");
 
-    if (currencySelect.value == 'dolar') {
-        currencyName.innerHTML = 'Dólar Americano'
-        currencyImage.src  = 'img/dolar.png'
-    }
-    if (currencySelect.value == 'euro') {
-        currencyName.innerHTML = 'Euro'
-        currencyImage.src = './img/euro.png'
-    }
-    convertValues()
+  if (currencySelect.value === "dolar") {
+    currencyName.innerHTML = "Dólar Americano";
+    currencyImage.src = "img/dolar.png";
+  }
+  if (currencySelect.value === "euro") {
+    currencyName.innerHTML = "Euro";
+    currencyImage.src = "./img/euro.png";
+  }
+  if (currencySelect.value === "libra") {
+    currencyName.innerHTML = "Libra";
+    currencyImage.src = "./img/libra.png"; 
+  }
+
+  convertValues();
 }
-currencySelect.addEventListener('change', changeCurrency)
 
-buttonConvert.addEventListener('click', convertValues)
+currencySelect.addEventListener("change", changeCurrency);
+
+buttonConvert.addEventListener("click", convertValues);
